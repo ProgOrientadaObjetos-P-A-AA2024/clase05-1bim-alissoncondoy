5262ed8cb56585227157edaba4ed15518e64cd73
alissoncondoy <adcondoy2@utpl.edu.ec>
2024-05-15 16:05:00 -0500

    cambios

---
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete02;
+import paquete01.LibretaCalificacion;
+/**
+ *
+ * @author utpl
+ */
+public class Ejecutor2 {
+    public static void main(String[] args) {
+        double [] notas = {10, 9, 8, 10};
+        String nombreEstudiante = "Rene Elizalde";
+        
+        LibretaCalificacion libreta2 = new LibretaCalificacion(nombreEstudiante,
+                notas);
+        
+        libreta2.establecerPromedio();
+        libreta2.establecerPromedioCualitativo();
+        
+        System.out.printf("\n%s\n", libreta2);        
+    }
+    
+}
